c6ad33839e6d0cf882efac488f696e23cfc61555
leebinjun <296735774@qq.com>
2020-01-04 18:37:55 +0800

    实现相册功能的尝试

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/lawlite19/blog-back-up/master/photos/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/leebinjun/leebinjun.github.io/dev/photos_backup/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/leebinjun/leebinjun.github.io/dev/photos_backup/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '';
