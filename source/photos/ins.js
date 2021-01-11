a6de9646da21500c77f89c1ea948d308b061b2d5
libj <libj@acca.com.cn>
2021-01-11 11:15:58 +0800

    photos图床由github变为七牛云

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -117,8 +117,8 @@
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
 
-          var minSrc = 'https://raw.githubusercontent.com/leebinjun/leebinjun.github.io/data/photos_backup/min_photos/' + data.link[i];
-          var src = 'https://raw.githubusercontent.com/leebinjun/leebinjun.github.io/data/photos_backup/photos/' + data.link[i];
+          var minSrc = 'http://img.binjun.xyz/photos/min/' + data.link[i];
+          var src = 'http://img.binjun.xyz/photos/src/' + data.link[i];
           var type = data.type[i];
           // var target = src + (type === 'video' ? '.mp4' : '.jpg');
           var target = src;
